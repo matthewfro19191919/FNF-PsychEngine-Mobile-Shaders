f35540405f589a559f1613a6011f78946fd6424c
Lily <egzozu.be.bas@gmail.com>
2025-02-28 12:05:18 +0300

    change hxdiscord_rpc for mobile

--- setup/mobile.sh
+++ setup/mobile.sh
@@ -14,7 +14,7 @@ haxelib install hscript-iris 1.1.3 --quiet
 haxelib install tjson 1.4.0 --quiet
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
 haxelib git linc_luajit https://github.com/MobilePorting/linc_luajit-0.7plus --quiet
-haxelib install hxdiscord_rpc 1.2.4 --quiet --skip-dependencies
+haxelib git hxdiscord_rpc https://github.com/MobilePorting/hxdiscord_rpc --quiet --skip-dependencies
 haxelib git hxvlc https://github.com/MobilePorting/hxvlc --quiet --skip-dependencies
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
