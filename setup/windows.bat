959bb395e366f2a5365039ac816cd7ddb27aab62
Lily <egzozu.be.bas@gmail.com>
2025-03-04 21:00:07 +0300

    no need anymor

--- setup/windows.bat
+++ setup/windows.bat
@@ -6,14 +6,14 @@ echo This might take a few moments depending on your internet speed.
 haxelib git hxcpp https://github.com/mcagabe19-stuff/hxcpp --quiet
 haxelib git lime https://github.com/mcagabe19-stuff/lime --quiet
 haxelib git openfl https://github.com/mcagabe19-stuff/openfl 9.3.3 --quiet
-haxelib install flixel 5.6.1 --quiet
+haxelib git flixel https://github.com/MobilePorting/flixel 5.6.1 --quiet
 haxelib install flixel-addons 3.2.2 --quiet
 haxelib install flixel-tools 1.5.1 --quiet
 haxelib install hscript-iris 1.1.3 --quiet
 haxelib install tjson 1.4.0 --quiet
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7 --quiet
-haxelib install hxdiscord_rpc 1.2.4 --quiet --skip-dependencies
+haxelib git linc_luajit https://github.com/MobilePorting/linc_luajit-0.7plus --quiet
+haxelib git hxdiscord_rpc https://github.com/MobilePorting/hxdiscord_rpc --quiet --skip-dependencies
 haxelib git hxvlc https://github.com/MobilePorting/hxvlc --quiet --skip-dependencies
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
