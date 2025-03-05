606c8c59fa0a4e49af0837c64f80528afe682320
Lily <egzozu.be.bas@gmail.com>
2025-03-05 12:31:30 +0300

    update lib links

--- setup/unix.sh
+++ setup/unix.sh
@@ -7,16 +7,16 @@ echo Makking the main haxelib and setuping folder in same time..
 mkdir ~/haxelib && haxelib setup ~/haxelib
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib git hxcpp https://github.com/mcagabe19-stuff/hxcpp --quiet
-haxelib git lime https://github.com/mcagabe19-stuff/lime --quiet
-haxelib git openfl https://github.com/mcagabe19-stuff/openfl 9.3.3 --quiet
+haxelib git hxcpp https://github.com/MobilePorting/hxcpp --quiet
+haxelib git lime https://github.com/MobilePorting/lime --quiet
+haxelib git openfl https://github.com/MobilePorting/openfl 9.3.3 --quiet
 haxelib git flixel https://github.com/MobilePorting/flixel 5.6.1 --quiet
 haxelib install flixel-addons 3.2.2 --quiet
 haxelib install flixel-tools 1.5.1 --quiet
 haxelib install hscript-iris 1.1.3 --quiet
 haxelib install tjson 1.4.0 --quiet
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
-haxelib git linc_luajit https://github.com/MobilePorting/linc_luajit-0.7plus --quiet
+haxelib git linc_luajit https://github.com/MobilePorting/linc_luajit --quiet
 haxelib git hxdiscord_rpc https://github.com/MobilePorting/hxdiscord_rpc --quiet --skip-dependencies
 haxelib git hxvlc https://github.com/MobilePorting/hxvlc --quiet --skip-dependencies
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
