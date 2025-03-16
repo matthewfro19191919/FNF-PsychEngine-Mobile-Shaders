5b0a73c918992d0e89fe0d0f672db5688feee594
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-03-16 07:16:17 -0400

    Update FunkinLua.hx

--- source/psychlua/FunkinLua.hx
+++ source/psychlua/FunkinLua.hx
@@ -1936,29 +1936,4 @@ class ModchartText extends FlxText
 		borderSize = 2;
 	}
 }
-
-class DebugLuaText extends FlxText
-{
-	private var disableTime:Float = 6;
-	public var parentGroup:FlxTypedGroup<DebugLuaText>;
-	public function new(text:String, parentGroup:FlxTypedGroup<DebugLuaText>, color:FlxColor) {
-		this.parentGroup = parentGroup;
-		super(10, 10, 0, text, 16);
-		setFormat(Paths.font("vcr.ttf"), 20, color, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		scrollFactor.set();
-		borderSize = 1;
-	}
-
-	override function update(elapsed:Float) {
-		super.update(elapsed);
-		disableTime -= elapsed;
-		if(disableTime <= 0) {
-			kill();
-			parentGroup.remove(this);
-			destroy();
-		}
-		else if(disableTime < 1) alpha = disableTime;
-	}
-
-}
 #end
